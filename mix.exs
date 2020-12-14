6b15415440eda45011dbb51de0351af9720dc7a5
Shairyar Baig <shairyarbaig@gmail.com>
2020-12-14 19:57:14 +0500

    Integrate AppSignal

--- mix.exs
+++ mix.exs
@@ -46,7 +46,8 @@ defmodule PhoenixDemo.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:appsignal_phoenix, "~> 2.0.0"}
     ]
   end
 
