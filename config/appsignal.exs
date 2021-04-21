1b7e076f144f1852df129513821f1006ea13d49a
Shairyar Baig <shairyarbaig@gmail.com>
2021-04-21 19:35:00 +0500

    Integrate AppSignal

--- config/appsignal.exs
+++ config/appsignal.exs
@@ -3,5 +3,7 @@ use Mix.Config
 config :appsignal, :config,
   otp_app: :phoenix_demo,
   name: "phoenix_demo",
-  push_api_key: "your-key",
+  push_api_key: "a1d7abab-dcbf-464b-a546-bb535e6ec73c",
+  debug: true,
+  transaction_debug_mode: true,
   env: Mix.env
