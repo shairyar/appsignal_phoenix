bdcc553df19eec0d2258dad8574d8a96c41b2433
Shairyar Baig <shairyarbaig@gmail.com>
2021-04-22 13:06:15 +0500

    Remove revision

--- config/appsignal.exs
+++ config/appsignal.exs
@@ -1,8 +1,6 @@
 use Mix.Config
 
-{revision, _exitcode} = System.cmd("git", ["log", "--pretty=format:%h", "-n 1"])
 config :appsignal, :config,
-  revision: revision,
   otp_app: :phoenix_demo,
   name: "phoenix_demo",
   push_api_key: "a1d7abab-dcbf-464b-a546-bb535e6ec73c",
